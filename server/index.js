6189eae92914f96e245b4c3575f25bf8c649f7c1
AMAN RAJAN SINGH <madhusingh74055@gmail.com>
2023-09-30 14:24:36 +0530

    Create user update Api

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 
@@ -10,6 +11,7 @@ dotenv.config();
 
 // Utility Middlewares
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use("/api/user", userRouter);
